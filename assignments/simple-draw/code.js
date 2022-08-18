c53f2d24e05d59a6adeb678e8852b7b8515ac31c
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-08-18 15:07:27 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -47,6 +47,6 @@ drawFilledRect(5, height - 20, 0, 0, 'purple');
 
 drawFilledCircle(width - 400, height - 500, 50, 'blue');
 
-drawLine(90, 90, 0, 0, 'red')
+drawLine(900, 90, 0, 0, 'red')
 
 drawLine(0, 0, width, height, 'red')
