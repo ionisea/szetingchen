2823845e069f9b1d1c9e91dde823717a453d4143
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-08-25 14:44:47 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -46,4 +46,4 @@ const endY = height
 
 drawLine (width, height, midX, midY, 'black', 2)
 drawLine (2*threeX, endY, midX, midY, 'black', 2)
-drawLine (endX, 2*threeY, midX, midY, 'black', 2)
+drawLine (4*threeX, endY, midX, midY, 'black', 2)
