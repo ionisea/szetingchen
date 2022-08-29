f8227b87e211436e72dcaa000ae0cca583723cbe
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-08-29 15:25:32 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -3,7 +3,7 @@ let position =
 drawFilledRect(0, 0, width, height, 'white');
 
 if (height/2) {
-    for (let y = height/2, x = 0; width - x > 1; y -= gap) {
+    for (let y = height, x = 0; width - x > 1; y -= gap) {
         x = y > 0 ? 0 : width * -y / (height - y);
         drawLine(x, Math.max(y, 0), width, height, 'black');
     }
