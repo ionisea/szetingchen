bc6455d5fe446840a2df023d2fc609bb65c23e82
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-08-29 15:09:30 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,49 +1,23 @@
-/*
- * This code is running in an environment with five functions defined:
- *
- *  drawLine(x1, y1, x2, y2, color, lineWidth)
- *
- *    Draws a line from x1,y1 to x2,y2 using the give color. The fifth argument,
- *    lineWidth, is optional and defaults to 1.
- *
- *  drawCircle(x, y, r, color, lineWidth=1)
- *
- *    Draws a circle centered at x,y with radius r using the given color. The
- *    fith argument, lineWidth, is optional and defaults to 1.
- *
- *  drawRect(x, y, width, height, color, lineWidth=1)
- *
- *    Draws a rectangle starting at x,y with the given width, height, and color.
- *    Positive widths go to the right and negative to the left; positive heights
- *    go down and negative heights go up. The sixth argument, lineWidth, is
- *    optional and defaults to 1.
- *
- *  drawFilledRect(x, y, width, height, color)
- *
- *    Draws a filled rectangle starting at x,y with the given width, height, and
- *    color. Positive widths go to the right and negative to the left; positive
- *    heights go down and negative heights go up.
- *
- *  drawFilledCircle(x, y, r, color)
- *
- *    Draws a filled circle centered at x,y with radius r using the given color.
- *
- * There are also a couple variables that might be useful:
- *
- *  width - the width of the drawing area.
- *  height - the height of the drawing area.
- *
- */
+drawFilledRect (0, 0, width, height, 'teal'); // use the red text to change the colors
+var shithead = 1000
+while (shithead >-200000) {
+    drawLine(0, shithead, width, height, 'maroon'); //you can do it here as well
+   // change the -x number to change the distance between circles, turn positive and funny little page crash
+    var shithead = shithead -4 //<- here!
+}
+//var fujkYou = 0
+//while (fujkYou <100000){
+  //  drawLine(0, fujkYou, width, height, 'maroon'); //you can do it here as well
+   // change the -x number to change the distance between circles, turn positive and funny little page crash
+  //  var fujkYou = fujkYou +10 //<- here!
+//}
+//drawFilledCircle (width/2, height/2, 150)
 
-const midX = width/2
-const midY = height/2
-const threeX = width/3
-const threeY = height/3
-const startX = width*0
-const startY = height*0
-const endX = width
-const endY = height
+// un mong us code dont touch vvvvv
 
-drawLine (width, height, midX, midY, 'black', 2)
-drawLine (2*threeX, endY, midX, midY, 'black', 2)
-drawLine (5*threeX, endY, midX, midY, 'black', 2)
+//drawFilledCircle (width/2,height/3, 100, "white")
+//drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'white');
+//drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
+//drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'white');
+//drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'white');
+//drawFilledRect (width/2 - 90, height/2 - 100, 100, 60, 'teal');
