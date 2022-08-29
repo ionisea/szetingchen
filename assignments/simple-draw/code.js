6483166b4617ffcda5b3ad797130ca33be11401c
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-08-29 15:11:07 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,23 +1,10 @@
-drawFilledRect (0, 0, width, height, 'teal'); // use the red text to change the colors
-var shithead = 1000
-while (shithead >-200000) {
-    drawLine(0, shithead, width, height, 'maroon'); //you can do it here as well
-   // change the -x number to change the distance between circles, turn positive and funny little page crash
-    var shithead = shithead -4 //<- here!
+const gap = 4;
+
+drawFilledRect(0, 0, width, height, 'white');
+
+for (let y = height, x = 0; width - x > 1; y -= gap) {
+    x = y > 0 ? 0 : width * -y / (height - y);
+    drawLine(x, Math.max(y, 0), width, height, 'black');
 }
-//var fujkYou = 0
-//while (fujkYou <100000){
-  //  drawLine(0, fujkYou, width, height, 'maroon'); //you can do it here as well
-   // change the -x number to change the distance between circles, turn positive and funny little page crash
-  //  var fujkYou = fujkYou +10 //<- here!
-//}
-//drawFilledCircle (width/2, height/2, 150)
 
-// un mong us code dont touch vvvvv
-
-//drawFilledCircle (width/2,height/3, 100, "white")
-//drawFilledRect (width/2 - 100, height/2 - 100, 200, 200, 'white');
-//drawFilledRect (width/2 + 100, height/2 - 100, 50, 130, 'white');
-//drawFilledRect (width/2 - 100, height/2 + 100, 60, 80, 'white');
-//drawFilledRect (width/2 + 40, height/2 + 100, 60, 80, 'white');
-//drawFilledRect (width/2 - 90, height/2 - 100, 100, 60, 'teal');
+drawFilledCircle(width / 2, height / 2, 150, 'rgba(255,0,255,0.25)');
