6fdb1fc203a51dc8e7dfa959aef12a5f6c9304c5
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-08-29 15:27:34 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -1,13 +1,10 @@
 const gap = 4;
-let position = 
+
 drawFilledRect(0, 0, width, height, 'white');
 
-if (height/2) {
-    for (let y = height, x = 0; width - x > 1; y -= gap) {
-        x = y > 0 ? 0 : width * -y / (height - y);
-        drawLine(x, Math.max(y, 0), width, height, 'black');
-    }
-} else {
-
+for (let y = height/2, x = 0; width - x > 1; y -= gap) {
+    x = y > 0 ? 0 : width * -y / (height - y);
+    drawLine(x, Math.max(y, 0), width, height, 'black');
 }
+
 //drawFilledCircle(width / 2, height / 2, 150, 'rgba(255,0,255,0.25)');
