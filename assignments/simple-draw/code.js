7e611158721a56c25e29416d6916449352d9b264
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-02 14:39:41 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -2,5 +2,5 @@ drawFilledRect (0, 0, 500,600, 'rgba(115,215,255,1)')
 var rect = 1000
 while (rect>= 2*height/3) {
   drawLine (0, rect, width, rect, 'rgba(0,128,0, 1')
-  var rect = rect - 2
+  var rect = rect - 4
 }
