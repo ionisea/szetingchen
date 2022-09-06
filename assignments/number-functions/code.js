f48b013eaa693e992c916441c0d04247e9d933cf
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-06 15:21:57 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -4,3 +4,15 @@ function add (a,b) {
 function subtract (a,b) {
   return a-b
 }
+function multiply (a,b) {
+  return a*b
+}
+function divide (a,b) {
+  return a/b
+}
+function mod (a,b) {
+  return a%b
+}
+function averageOfTwo (a,b) {
+  return (a+b)/2
+}
