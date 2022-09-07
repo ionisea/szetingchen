d5aa013cadd5ba32053b51949b08f572e18dcf6e
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-07 14:46:06 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -15,9 +15,9 @@ while (rays >= width-width/7) {
   drawLine (0, 0, rays, height, 'rgba(242,242,37,.1', 1)
   var rays = rays - 1
 }
-//var rays2 = 700
-//while (rays2 >= width*0-width/7) {
-//  drawLine (0, 0, rays2, height, 'rgba(242,242,37,.1', 1)
-//  var rays2 = rays2 - 1
-//}
+var rays2 = 200
+while (rays2 >= width*0) {
+  drawLine (0, 0, rays2, height, 'rgba(242,242,37,.1', 1)
+  var rays2 = rays2 - 1
+}
 drawFilledCircle (0,0, 100,'rgba(250,220,15,1)')
