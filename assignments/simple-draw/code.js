1bea8acb290cec5b4cf1d8ecce1ed08ea1121843
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-07 14:45:14 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -10,7 +10,7 @@ while (rect>= 2*height/3) {
   }
 }
 
-var rays = 1000
+var rays = 200
 while (rays >= width-width/7) {
   drawLine (0, 0, rays, height, 'rgba(242,242,37,.1', 1)
   var rays = rays - 1
