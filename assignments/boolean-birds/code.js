68210d171b6df5c5dbcd083c8d976ec1f9499616
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-08 15:32:20 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -50,5 +50,5 @@ const eatsMice = (isBirdRed, isBirdSpotted) => {
 }
 
 const isRed = (bird) => {
-  return bird ==! 'Flobby' ==! 'Bloggy' === 'Flibble' === 'Globby'
+  return bird === 'Flobby' || bird === 'Bloggy'
 }
