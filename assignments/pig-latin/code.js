39a89ed7f6117e7e233c3a2ed61e9408f98c0e29
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-14 14:43:54 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -1,43 +1,9 @@
-// A new method you will probably want to use.
-//
-// If s is a string:
-//
-//  s.search(/[aeiou]/) returns the index in s of the first vowel.
-//
-// For instance
-//
-//  const s = 'phone';
-//
-//  s.search(/[aeiou]/) returns 2
-//
-// The funny syntax /[aeiou]/ is called a "regular expression". We'll learn
-// about them in more detail later.
-//
-// To write the advancedPigLatin function you will want to know about a new
-// operator called the "ternary operator". (It's called this because it has
-// three operands.)
-//
-// It looks like:
-//
-//  booleanValue ? value1 : value2
-//
-// And it evaluates to value1 is booleanValue is true and value2 if it is false.
-//
-// For example:
-//
-//  x === 0 ? "Zero" : "Not zero"
-//
-// will evaluate to either "Zero" or "Not zero" depending on whether x === 0.
-//
-// Feel free to play around in the REPL to make sure you understand how this
-// operator works.
+const v = s.search(/[aeiou]/)
 
 const pigLatin = (s) => {
-  var v = s.search(/[aeiou]/)
-  return s.substring(v,s.length) + s.substring(0,v) + 'ay'
+  return s.substring(v, s.length) + s.substring(0, v) + 'ay'
 }
 
 const advancedPigLatin = (s) => {
-  var v = s.search(/[aeiou]/)
-  return v === 0 ? s+'way' : s.substring(v,s.length) + s.substring(0,v) + 'ay'
+  return v === 0 ? s + 'way' : s.substring(v, s.length) + s.substring(0, v) + 'ay'
 }
