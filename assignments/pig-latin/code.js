eaa16b7931c8f9716795d77428f5ae097f5bd0e3
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-14 14:45:16 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -1,9 +1,9 @@
-let v = s.search(/[aeiou]/)
-
 const pigLatin = (s) => {
+  let v = s.search(/[aeiou]/)
   return s.substring(v, s.length) + s.substring(0, v) + 'ay'
 }
 
 const advancedPigLatin = (s) => {
+  let v = s.search(/[aeiou]/)
   return v === 0 ? s + 'way' : s.substring(v, s.length) + s.substring(0, v) + 'ay'
 }
