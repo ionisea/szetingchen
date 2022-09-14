95a5d946172c31dc860e66d4a4ea8560f6130129
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-14 15:12:52 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,11 +1,12 @@
 const radius = 23
-let dist = radius/2
+const diameter = 2*radius
+let dist = diameter
 while (dist < width){
-  dist = dist+radius/2
+  dist = dist+diameter
 }
 const center = width-dist
-dist = center+radius/2
+dist = center+diameter
 while (dist < width){ 
   drawFilledCircle(dist-center/2, height/2, bigness, 'red')
-  dist = dist+radius/2
+  dist = dist+diameter
 }
