fccf3c4f1fc4e3b71599cff395ffceed5ade96f5
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-14 15:10:17 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,38 +1,12 @@
-/*
- * This code is running in an environment the same as simple-draw. Thus you have
- * two variables that will be helpful.
- *
- *  width - the width of the drawing area.
- *  height - the height of the drawing area.
- *
- * And these methods which do the same thing as in simple-draw.
- *
- *  drawLine(x1, y1, x2, y2, color, lineWidth)
- *
- *  drawCircle(x, y, radius, color, lineWidth=1)
- *
- *  drawRect(x, y, w, h, color, lineWidth=1)
- *
- *  drawTriangle(x1, y1, x2, y2, x3, y3, color, lineWidth=1)
- *
- *  drawFilledCircle(x, y, r, color)
- *
- *  drawFilledRect(x, y, width, height, color)
- *
- *  drawFilledTriangle(x1, y1, x2, y2, x3, y3, color)
- *
- *  clear()
- */
-
 const bigness = 23
-let dist = bigness*2
+const radius = bigness*2
+let dist = radius
 while (dist < width){
-  dist = dist+bigness*2
+  dist = dist+radius
 }
 const center = width-dist
-dist = center+bigness*2
+dist = center+radius
 while (dist < width){ 
   drawFilledCircle(dist-center/2, height/2, bigness, 'red')
-  dist = dist+bigness*2
+  dist = dist+radius
 }
-
