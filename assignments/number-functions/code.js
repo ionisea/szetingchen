91f6f1e4311909b6d7bba2597fd07007bf8ebc6f
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-14 14:39:51 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -1,31 +1,31 @@
-function add (a,b) {
+const add = (a,b) => {
   return a+b
 }
-function subtract (a,b) {
+const subtract = (a,b) => {
   return a-b
 }
-function multiply (a,b) {
+const multiply = (a,b) => {
   return a*b
 }
-function divide (a,b) {
+const divide = (a,b) => {
   return a/b
 }
-function mod (a,b) {
+const mod = (a,b) => {
   return a%b
 }
-function averageOfTwo (a,b) {
+const averageOfTwo = (a,b) => {
   return (a+b)/2
 }
-function averageOfThree (a,b,c) {
+const averageOfThree = (a,b,c) => {
   return (a+b+c)/3
 }
-function distance (a,b) {
+const distance = (a,b) => {
   return Math.abs(a-b)
 }
-function manhattanDistance (a,b,c,d) {
+const manhattanDistance = (a,b,c,d) => {
   return Math.abs(a-c)+Math.abs(b-d)
 }
-function euclideanDistance (a,b,c,d) {
+const euclideanDistance = (a,b,c,d) => {
   return Math.sqrt(((Math.abs(a-c))**2)+((Math.abs(b-d))**2))
 }
 
