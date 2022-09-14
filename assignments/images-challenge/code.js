c3c5bd5ad63632ee76a71f86ed0c49feb4b585c0
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-14 15:29:26 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -3,7 +3,7 @@ const diameter = 2*radius
 const dist = Math.floor(width/diameter)*diameter
 const extra = width-dist
 let x = extra/2
-while (x < width){ 
+while (x < width-radius){ 
   drawFilledCircle(x+extra/2, height/2, radius, 'red')
   x += diameter
 }
