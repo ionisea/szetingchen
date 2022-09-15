816ae8ce33997b8d48724a25a70fcdbba852eaa0
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-15 14:42:17 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,9 +1,22 @@
+/*
 const radius = 29
 const diameter = 2*radius
 const dist = Math.floor(width/diameter)*diameter
 const extra = width-dist
 let x = 0
 while (x < width-extra){ 
+  drawFilledCircle(x+radius+extra/2, height/2, radius, 'red')
+  x += diameter
+}
+*/
+
+const lineOfCircles = (29) => {
+  const radius = 29
+  const diameter = 2*radius
+  const dist = Math.floor(width/diameter)*diameter
+  const extra = width-dist
+  let x = 0
+  while (x < width-extra){ 
   drawFilledCircle(x+radius+extra/2, height/2, radius, 'red')
   x += diameter
 }
