3b7bbd6d559d9c59eaceee1e7317bbde2ffa648e
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-20 14:41:29 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -103,6 +103,6 @@ concentricCircles (amountCircles);
 */
 
 const checkerboard = (n) => {
-  drawRect (200*n, 200*n, 0, 0, 'red')
+  drawRect (200*n, 200*n, width/2, height/2, 'red')
 }
 checkerboard (n);
