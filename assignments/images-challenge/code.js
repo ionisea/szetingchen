fd3784613502d64f787d630e9155969dfa9b32c1
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-20 14:56:59 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -108,10 +108,10 @@ const checkerboard = (n) => {
 }
 checkerboard (n);
 */
-const fillwithcircles = () => {
+const lineofcircles = () => {
 for (let x = 0; x < 50; x++) 
 if (x > length) {
  drawFilledCircle(x*10, height/2 , 5, 'red', 1)
 }
  }
-fillwithcircles();
+lineofcircles();
