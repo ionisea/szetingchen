71087f4275a88291c0da4f77d3e1c2dcf382467a
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-21 15:33:53 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -130,14 +130,12 @@ const checkerboard = (n) => {
       x1 += 2 * squareSide
     }
     y += squareSide
-    if (y < width) {
-      let x2 = squareSide
-      while (x2 < width) {
-        drawFilledRect(x2 + 0, y + heightextra, squareSide, squareSide, 'blue')
-        x2 += 2 * squareSide
-      }  
-      y += squareSide
-    } 
+    let x2 = squareSide
+    while (x2 < width) {
+      drawFilledRect(x2 + 0, y + heightextra, squareSide, squareSide, 'blue')
+      x2 += 2 * squareSide
+    }  
+    y += squareSide
   }
 }
 checkerboard(77);
