77efad3c16f4ff85b59fdf91248de798b0080d13
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-21 15:02:19 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -134,7 +134,7 @@ const checkerboard = (n) => {
   const heightextra = (height-width)/2
   drawFilledRect (0, heightextra, width, width, 'red')
   let y = 0
-  while (y < width-heightextra) {
+  while (y < width-2*heightextra) {
     let x1 = 0
     while (x1 < width-heightextra) {
       drawFilledRect (x1+0, y+heightextra, squareSide, squareSide, 'blue')
