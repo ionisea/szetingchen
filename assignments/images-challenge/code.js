a79e9d899243c86faf201e64d89d477bee111520
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-22 15:00:24 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -25,12 +25,12 @@
  */
 
 const lineOfCircles = (radius) => {
-  const diameter = 2*radius
-  const dist = Math.floor(width/diameter)*diameter
-  const extra = width-dist
+  const diameter = 2 * radius
+  const dist = Math.floor(width / diameter) * diameter
+  const extra = width - dist
   let x = 0
-  while (x < width-extra){ 
-    drawFilledCircle(x+radius+extra/2, height/2, radius, 'red')
+  while (x < width - extra) {
+    drawFilledCircle(x + radius + extra / 2, height / 2, radius, 'red')
     x += diameter
   }
 }
@@ -44,9 +44,9 @@ const lineOfAltCircles = (radius) => {
   let x = 0
   while (x < width - extra) {
     if (color === 'red') {
-    color = 'blue'
+      color = 'blue'
     } else {
-    color = 'red'
+      color = 'red'
     }
     drawFilledCircle(x + radius + extra / 2, height / 2, radius, color)
     x += diameter
@@ -55,16 +55,16 @@ const lineOfAltCircles = (radius) => {
 //lineOfAltCircles (23);
 
 const fillWithCircles = (radius) => {
-  const diameter = 2*radius
-  const xdist = Math.floor(width/diameter)*diameter
-  const xextra = width-xdist
-  const ydist = Math.floor(height/diameter)*diameter
-  const yextra = height-ydist
+  const diameter = 2 * radius
+  const xdist = Math.floor(width / diameter) * diameter
+  const xextra = width - xdist
+  const ydist = Math.floor(height / diameter) * diameter
+  const yextra = height - ydist
   let y = 0
-  while (y < height-yextra) {
+  while (y < height - yextra) {
     let x = 0
-    while (x < width-xextra) {
-      drawCircle(x+radius+xextra/2, y+radius+yextra/2, radius, 'blue')
+    while (x < width - xextra) {
+      drawCircle(x + radius + xextra / 2, y + radius + yextra / 2, radius, 'blue')
       x += diameter
     }
     y += diameter
@@ -73,41 +73,41 @@ const fillWithCircles = (radius) => {
 //fillWithCircles (23);
 
 const fillWithCirclesRand = (radius, probability) => {
-  const diameter = 2*radius
-  const xdist = Math.floor(width/diameter)*diameter
-  const xextra = width-xdist
-  const ydist = Math.floor(height/diameter)*diameter
-  const yextra = height-ydist
+  const diameter = 2 * radius
+  const xdist = Math.floor(width / diameter) * diameter
+  const xextra = width - xdist
+  const ydist = Math.floor(height / diameter) * diameter
+  const yextra = height - ydist
   let y = 0
-  while (y < height-yextra) {
+  while (y < height - yextra) {
     let x = 0
-    while (x < width-xextra) {
+    while (x < width - xextra) {
       let rand = Math.random()
       if (rand > probability) {
-        drawCircle(x+radius+xextra/2, y+radius+yextra/2, radius, 'blue')
+        drawCircle(x + radius + xextra / 2, y + radius + yextra / 2, radius, 'blue')
       } else {
-        drawFilledCircle(x+radius+xextra/2, y+radius+yextra/2, radius, 'blue')
+        drawFilledCircle(x + radius + xextra / 2, y + radius + yextra / 2, radius, 'blue')
       }
       x += diameter
-      }
+    }
     y += diameter
   }
 }
 //fillWithCirclesRand (23, 0.2);
 
 const concentricCircles = (amountCircles) => {
-  drawFilledCircle (width/2, height/2, width/2, 'blue')
-  const radiuses = (width/2)/(amountCircles)
+  drawFilledCircle(width / 2, height / 2, width / 2, 'blue')
+  const radiuses = (width / 2) / (amountCircles)
   let p = 0
   let color = 'red'
   let x = 0
-  while (x < width/2) {
+  while (x < width / 2) {
     if (color === 'red') {
       color = 'blue'
     } else {
       color = 'red'
     }
-    drawFilledCircle(width/2, height/2, width/2-p, color)
+    drawFilledCircle(width / 2, height / 2, width / 2 - p, color)
     p += radiuses
   }
 }
@@ -119,10 +119,10 @@ const checkerboard = (n) => {
   drawFilledRect(0, heightextra, width, width, 'red')
   let startpos = 0
   for (let y = 0; y < width; y += squareSide) {
-    for (let x = startpos; x < width; x += 2*squareSide) {
+    for (let x = startpos; x < width; x += 2 * squareSide) {
       drawFilledRect(x + 0, y + heightextra, squareSide, squareSide, 'blue')
     }
     startpos += squareSide
   }
 }
-checkerboard(100);
+checkerboard(3);
