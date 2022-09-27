fb339ebe51ced9e129b130677d2c128ddb469cf0
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-27 15:20:29 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -1,29 +1,3 @@
-/*
- * This code is running in an environment the same as simple-draw. Thus you have
- * two variables that will be helpful.
- *
- *  width - the width of the drawing area.
- *  height - the height of the drawing area.
- *
- * And these methods which do the same thing as in simple-draw.
- *
- *  drawLine(x1, y1, x2, y2, color, lineWidth)
- *
- *  drawCircle(x, y, radius, color, lineWidth=1)
- *
- *  drawRect(x, y, w, h, color, lineWidth=1)
- *
- *  drawTriangle(x1, y1, x2, y2, x3, y3, color, lineWidth=1)
- *
- *  drawFilledCircle(x, y, r, color)
- *
- *  drawFilledRect(x, y, width, height, color)
- *
- *  drawFilledTriangle(x1, y1, x2, y2, x3, y3, color)
- *
- *  clear()
- */
-
 const lineOfCircles = (radius) => {
   const diameter = 2 * radius
   const dist = Math.floor(width / diameter) * diameter
@@ -127,6 +101,7 @@ const concentricCircles = (amountCircles) => {
   }
 }
 concentricCircles(13);
+//I'm thinking of a solution to the if/else massive loop problem, so I'll leave this as is for now
 
 const checkerboard = (n) => {
   if (height <= width) {
