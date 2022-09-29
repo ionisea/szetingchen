2e5ab4a3edf3e03cd8d80b42c3dd4f64961db215
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-09-29 15:26:46 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -104,7 +104,7 @@ const checkerboard = (n) => {
     }
   }
 }
-checkerboard(2);
+checkerboard(8);
 
 const notReallyCurved = (lines) => {
   for (let position = 0; position != lines + 1; position++) {
