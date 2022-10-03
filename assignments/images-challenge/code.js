5ade39c3b66bcb8122769190eb0e6fcb7021fa58
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-03 15:08:52 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -115,7 +115,7 @@ const squareOfCircles = (radius, color) => {
     drawCircle(xCheese + cExtra / 2, yPos, radius, color)
   }
 }
-squareOfCircles(45, 'blue');
+squareOfCircles(25, 'blue');
 
 const z_sqr = (x, y) => {
   return [x ** 2 - y ** 2, 2 * x * y];
