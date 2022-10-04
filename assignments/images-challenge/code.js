e380162b40ec7c6eabd7361e3f5c13c0b9ad3be4
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-04 15:16:43 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -108,7 +108,7 @@ const squareOfCircles = (radius, color) => {
     drawCircle(xPos, radius + heightExtra, radius, color)
   }
   for (let yPos = 3 * radius; yPos < height - diameter; yPos += diameter) {
-    drawCircle((width - height + diameter) / 2 + heightExtra, yPos, radius, color)
+    drawCircle((width - height + diameter) / 2 - heightExtra, yPos, radius, color)
     drawCircle((width + height - diameter) / 2 + heightExtra, yPos, radius, color)
   }
 }
