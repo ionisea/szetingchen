f41cc2410830269c9c36a89155417852e30a99d8
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-05 15:26:21 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -88,7 +88,7 @@ const checkerboard = (n, color1, color2) => {
     startpos = startpos === 0 ? squareSide : 0
   }
 }
-checkerboard(15, 'black', 'white'); //currently set to a chessboard (black/white)
+//checkerboard(15, 'black', 'white'); //currently set to a chessboard (black/white)
 
 const notReallyCurved = (lines) => {
   const gap = height / lines
@@ -111,7 +111,7 @@ const squareOfCircles = (radius, color) => {
     drawCircle((width + height - diameter) / 2 + heightExtra, yPos, radius, color)
   }
 }
-//squareOfCircles(45, 'blue');
+squareOfCircles(45, 'blue');
 
 const z_sqr = (x, y) => {
   return [x ** 2 - y ** 2, 2 * x * y];
