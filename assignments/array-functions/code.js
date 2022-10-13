0502e04f75d3357f02fc7e036e2c037032ff300b
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-13 15:18:05 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,10 @@
 const timesTable = (row) => {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  for(const element of numbers) {
+  const numbers = []
+  for (let x = 0; x < row; x++) {
+    numbers.push (x)
+  }
+  for (const element of numbers) {
     console.log(element * row)
   }
-};
-timesTable (9);
+}
+timesTable(9);
