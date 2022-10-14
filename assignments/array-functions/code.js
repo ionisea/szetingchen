487ec2ae9756feba8975072f0870e0697d79262e
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-14 14:50:23 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,6 +1,6 @@
 const timesTable = (rows, columns) => {
   const numbers = Array(rows).fill().map((_, i) => i + 1)
-  for (i = 0; i <= columns; i++) {
+  for (let i = 0; i <= columns; i++) {
     const print = numbers.map(x => x * element)
     console.log(print)
   }
