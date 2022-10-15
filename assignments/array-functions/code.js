92db784bc90d30bfd986b6f9e83cdaf50382b89a
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-14 20:38:30 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -10,7 +10,7 @@ const timesTable = (rows, columns) => {
 const countTens = (array) => {
   let num = 0
   for (const element in array) {
-    if (element === 10) {
+    if (element == 10) {
       num++
     }
   }
