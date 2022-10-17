1bc48c0ae42cf3e80045c229119eb4ac419637fe
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-17 15:19:39 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -12,6 +12,6 @@ drawLine (max/2 + (min*0.3), 0, max/2 + (min*0.3), min, 'black', 7)
 drawLine (max/2 - (min*0.3), 0, max/2 - (min*0.3), min, 'black', 7)
 let player = 'X'
 registerOnclick((x, y) => {
-  drawText(player, x, y, 'black', Math.min(width, height) * 0.3);
+  drawText(player, x-25, y+25, 'black', Math.min(width, height) * 0.3);
   player == 'X' ? player = 'O' : player = 'X'
 });
