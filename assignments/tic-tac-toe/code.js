7b94c1aa07468b37c1aeff703f3319855b314203
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-17 15:18:07 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,6 +5,10 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 
+const min = Math.min(width, height)
+const max = Math.max(width, height)
+
+drawLine (max/2 + (min*0.3), 0, max/2 - (min*0.3), min, 5)
 let player = 'X'
 registerOnclick((x, y) => {
   drawText(player, x, y, 'black', Math.min(width, height) * 0.3);
