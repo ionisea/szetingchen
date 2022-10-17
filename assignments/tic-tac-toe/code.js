acf2cc156e43d6d943c5fcb8990eaf5f5a7c5b84
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-17 15:07:10 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,3 +8,7 @@
 registerOnclick((x, y) => {
   drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
 });
+
+registerOnclick((x, y) => {
+  drawText('O', x, y, 'black', Math.min(width, height) * 0.3);
+});
