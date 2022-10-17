e83dabc2bdbf741d4bc91ec23e0f4ff0f36bb5c8
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-17 15:20:23 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,8 +8,8 @@
 const min = Math.min(width, height)
 const max = Math.max(width, height)
 
-drawLine (max/2 + (min*0.3), 0, max/2 + (min*0.3), min, 'black', 7)
-drawLine (max/2 - (min*0.3), 0, max/2 - (min*0.3), min, 'black', 7)
+drawLine (max/2 + (min*0.2), 0, max/2 + (min*0.3), min, 'black', 7)
+drawLine (max/2 - (min*0.2), 0, max/2 - (min*0.3), min, 'black', 7)
 let player = 'X'
 registerOnclick((x, y) => {
   drawText(player, x-25, y+25, 'black', Math.min(width, height) * 0.3);
