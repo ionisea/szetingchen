23ac69b5cfe0d5d8ebacdb321e507897bbf617b2
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-18 14:44:15 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,10 +8,17 @@
 const min = Math.min(width, height)
 const max = Math.max(width, height)
 
+for (let lines = 0; lines < 4; lines++) {
+  drawLine (max/2 -min/2 + lines, max, max/2 - min/2, lines)
+}
+
+/*
 drawLine(max / 2 + (min * 0.2), 0, max / 2 + (min * 0.2), min, 'black', 7)
 drawLine(max / 2 - (min * 0.2), 0, max / 2 - (min * 0.2), min, 'black', 7)
 drawLine(max / 2 + (min * 0.5), min / 3, max / 2 - (min * 0.5), min / 3, 'black', 7)
-drawLine(max / 2 + (min * 0.5), 2*(min / 3), max / 2 - (min * 0.5), 2*(min / 3), 'black', 7)
+drawLine(max / 2 + (min * 0.5), 2*(min / 3), max / 2 - (min * 0.5), 2*(min / 3), 'black', 7)\
+*/
+
 let player = 'X'
 registerOnclick((x, y) => {
   drawText(player, x - 25, y + 25, 'black', Math.min(width, height) * 0.3);
