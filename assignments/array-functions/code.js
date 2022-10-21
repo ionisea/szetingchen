4ee85f755a1a4265c619ec5ca48d1147a944e855
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-21 14:09:28 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -16,7 +16,7 @@ const exponentsTable = (rows, columns) => {
 
 const fibonacci = (num) => {
   const array = [0, 1, 1]
-  for (let i = 0; i < num - 2; i++) {
+  for (let i = 0; i < num; i++) {
     array.push(array[i] + array[i + 1])
   }
   return array
