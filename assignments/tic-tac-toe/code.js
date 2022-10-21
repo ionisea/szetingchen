4507dfc8282e86b4de3adf5d8c4470971751e6eb
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-21 15:22:53 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -25,7 +25,7 @@ registerOnclick((x, y) => {
   if (turns < 9) {
     drawText(player, x - (min / 10), y + (min / 10), 'black', min * 0.3)
     turns++
-    console.log('player:' + player, 'turn:' + turns, 'coords: ' + Math.ceil(width / x), Math.ceil(height / y))
+    console.log('player:' + player, 'turn:' + turns, 'coords: ' + Math.ceil(width / (2*x)), Math.ceil(height / (2*y)))
     player == 'O' ? player = 'X' : player = 'O'
   }
 });
