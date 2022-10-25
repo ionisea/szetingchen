a22cde5215212b1080d3fd13a09f7d19bfa65622
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-25 14:49:42 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -19,15 +19,6 @@ registerOnclick((x, y) => {
     if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
       return console.log('dumbass')
     }
-    /*
-    if (x < max / 2 - min / 6) {
-      xPos = 0
-    } else if (x < max / 2 + min/ 6) {
-      xPos = 1
-    } else if (x < max / 2 + min / 2) {
-      xPos = 2
-    }
-    */
     y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
     x < max / 2 - min /6 ? xPos = 0 :  x < max / 2 + min/ 6 ? xPos = 1 : xPos = 2
     console.log(coordArray[yPos][xPos])
