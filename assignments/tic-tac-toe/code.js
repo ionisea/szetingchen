7ff8ddd99effc93835fac964da2b9be1612f7836
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-25 14:36:23 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -5,31 +5,50 @@
 // is called an anonymous function. We'll discuss this in more detail in a few
 // weeks but for now you can just adapt this code.
 
-const min = Math.min(width, height)
-const max = Math.max(width, height)
-
-for (let lines = 0; lines < 4; lines++) {
-  drawLine(max / 2 - min / 2 + (lines * min / 3), min, max / 2 - min / 2 + (lines * min / 3), 0, 'black', 2)
-  drawLine(max / 2 - min / 2, lines * min / 3, max / 2 + min / 2, lines * min / 3, 'black', 2)
-}
-
-const board = [
+const coordArray = [
   [[], [], []],
   [[], [], []],
   [[], [], []],
 ]
-
-let turns = 0
-let player = 'X'
+const max = Math.max(width, height)
+const min = Math.min(width, height)
+const yPosConst = min * 0.1 + min / 6
+const xPosConst = max / 2 - min / 2 - min * 0.1 + min / 6
+let player = 'X';
+for (let editConst = 0; editConst <= 1; editConst += 1 / 3) {
+  drawLine(max / 2 - min / 2 + min * editConst, height, max / 2 - min / 2 + min * editConst, 0, 'black', 2)
+  drawLine(max / 2 - min / 2, height * editConst, max / 2 + min / 2, height * editConst, 'black', 2)
+}
+let turns = 0;
+let xPos;
+let yPos;
 registerOnclick((x, y) => {
   if (turns < 9) {
-    drawText(player, x - (min / 10), y + (min / 10), 'black', min * 0.3)
     turns++
-    console.log('player:' + player, 'turn:' + turns, 'coords: ' + x, y)
-    player == 'O' ? player = 'X' : player = 'O'
+    //player == 'X' ? player = 'O' : player = 'X';
+    console.log('turn: ' + turns, 'coords: ', x, y)
+    if (y < 1 / 3 * height) {
+      yPos = yPosConst
+    } else if (y < 2 / 3 * height) {
+      yPos = yPosConst + 1 / 3 * height
+    } else if (y < height) {
+      yPos = yPosConst + 2 / 3 * height
+    }
+    if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
+      return console.log('dumbass')
+    }
+    if (x < max / 2 - min / 2 + 1 / 3 * min) {
+      xPos = xPosConst
+    } else if (x < max / 2 + min * 1 / 6) {
+      xPos = xPosConst + 1 / 3 * min
+    } else if (x < max / 2 + min / 2) {
+      xPos = xPosConst + 2 / 3 * min
+    }
+    console.log('turn: ' + turns, 'coords: ', x, y)
+    drawText(player, xPos, yPos, 'black', min * 0.33);
+    player == 'X' ? player = 'O' : player = 'X';
   }
 });
-
-const xPos = 1
-const yPos = 2
-drawText(player, max/2-min/2 - min*0.1 + (min * xPos/3)+min/6, min*0.1 + min*yPos/3 + min/6, 'black', min * 0.3)
+//const yPos = 0 // 0, 1, 2
+//const xPos = 0 // 0, 1, 2 
+//drawText(player, max/2-min/2 - min*0.1 +min/6 + (min * xPos/3), min*0.1 + min/6 + min*yPos/3, 'black', min * 0.3)
