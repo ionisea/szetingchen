10802d171dcae785be27c97ade5e2b05b4e3e14e
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-25 14:47:34 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,20 +1,11 @@
-// This is a bit of a new thing. registerOnclick is a function provided by the
-// framework. But the argument we're passing to it is *another* function. Notice
-// how the argument here looks like what we normally put on the righthand side
-// of the equals sign in our normal `const foo = ...` function definition. This
-// is called an anonymous function. We'll discuss this in more detail in a few
-// weeks but for now you can just adapt this code.
-
 const coordArray = [
   [[], [], []],
-  [[], [], []],
+  [[], ['X'], []],
   [[], [], []],
 ]
 const max = Math.max(width, height)
 const min = Math.min(width, height)
-const yPosConst = min * 0.1 + min / 6
-const xPosConst = max / 2 - min / 2 - min * 0.1 + min / 6
-let player = 'X';
+
 for (let editConst = 0; editConst <= 1; editConst += 1 / 3) {
   drawLine(max / 2 - min / 2 + min * editConst, height, max / 2 - min / 2 + min * editConst, 0, 'black', 2)
   drawLine(max / 2 - min / 2, height * editConst, max / 2 + min / 2, height * editConst, 'black', 2)
@@ -22,31 +13,25 @@ for (let editConst = 0; editConst <= 1; editConst += 1 / 3) {
 let turns = 0;
 let xPos;
 let yPos;
+let player = 'X';
 registerOnclick((x, y) => {
   if (turns < 9) {
-    turns++
-    if (y < 1 / 3 * height) {
-      yPos = yPosConst
-    } else if (y < 2 / 3 * height) {
-      yPos = yPosConst + 1 / 3 * height
-    } else if (y < height) {
-      yPos = yPosConst + 2 / 3 * height
-    }
     if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
       return console.log('dumbass')
     }
-    if (x < max / 2 - min / 2 + 1 / 3 * min) {
-      xPos = xPosConst
-    } else if (x < max / 2 + min * 1 / 6) {
-      xPos = xPosConst + 1 / 3 * min
+    if (x < max / 2 - min / 2 + min/3) {
+      xPos = 0
+    } else if (x < max / 2 + min/ 6) {
+      xPos = 1
     } else if (x < max / 2 + min / 2) {
-      xPos = xPosConst + 2 / 3 * min
+      xPos = 2
     }
+    y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
+    //x < max / 2 - min / 2 + 1 / 3 * min ? xPos = 0 :  x < max / 2 + min * 1 / 6
+    console.log(coordArray[yPos][xPos])
+    turns++
     console.log('turn: ' + turns, 'coords: ', x, y)
-    drawText(player, xPos, yPos, 'black', min * 0.27);
+    drawText(player, max / 2 - min / 2 - min * 0.1 + min / 6 + (min * xPos / 3), min * 0.1 + min / 6 + min * yPos / 3, 'black', min * 0.3)
     player == 'X' ? player = 'O' : player = 'X';
   }
 });
-//const yPos = 0 // 0, 1, 2
-//const xPos = 0 // 0, 1, 2 
-//drawText(player, max/2-min/2 - min*0.1 +min/6 + (min * xPos/3), min*0.1 + min/6 + min*yPos/3, 'black', min * 0.3)
