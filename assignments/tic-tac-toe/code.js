8f517c7deb8f4193987faf2c6d7fff22457d58fd
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-25 15:29:11 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -1,4 +1,4 @@
-drawFilledRect (0, 0, width, height, 'rgba(115,175,255,1)')
+drawFilledRect(0, 0, width, height, 'rgba(115,175,255,1)')
 const coordArray = [
   [[], [], []],
   [[], [], []],
@@ -19,17 +19,18 @@ registerOnclick((x, y) => {
   if (turns < 9) {
     if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
       for (let i = 0; i < 250000; i++)
-      console.log('dumbass')
-    }
-    y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
-    x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
-    if (coordArray[yPos][xPos] == '') {
-      coordArray[yPos][xPos].push(player)
-      turns++
-      console.log('turn: ' + turns, 'coords: ', x, y)
-      drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3)
-      player == '⬜' ? player = '⬛' : player = '⬜';
-      console.log(coordArray)
+        console.log(Math.random() + 'fujk yu')
+    } else {
+      y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
+      x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
+      if (coordArray[yPos][xPos] == '') {
+        coordArray[yPos][xPos].push(player)
+        turns++
+        console.log('turn: ' + turns, 'coords: ', x, y)
+        drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3)
+        player == '⬜' ? player = '⬛' : player = '⬜';
+        console.log(coordArray)
+      }
     }
   }
 });
