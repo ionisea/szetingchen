80fef3bc5972cc45ad6313158ac3a6008a417240
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-26 14:37:16 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -18,12 +18,13 @@ let player = '⬜';
 registerOnclick((x, y) => {
   if (turns < 9) {
     if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
+      /*
       for (let i = 0; i < 250000; i++)
         if (Math.random() < 0.5) {
         console.log(Math.random() + '  fujk yu')
        } else {
         console.log((-1)*(Math.random()) + '   fokin hel')
-       }
+       }*/
     } else {
       y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
       x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
