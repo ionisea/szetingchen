eb0aa5009098b85d0e93cea9cc18c128238670bc
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-26 15:26:02 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -23,15 +23,15 @@ registerOnclick((x, y) => {
     let yee = 0;
     let pee = 1;
     let lee = 1;
-    for (let i = 0; i < 25000000; i++) {
+    for (let i = 0; i < 250000000; i++) {
       ree = Math.random()
       yee < ree ? yee = ree : 0
     }
-    for (let i = 0; i < 25000000; i++) {
+    for (let i = 0; i < 250000000; i++) {
       pee = Math.random()
       lee > pee &&! pee == 0? lee = pee : 0
     }
-    console.log(yee*lee)
+    console.log(yee/lee)
   } else {
     y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
     x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
