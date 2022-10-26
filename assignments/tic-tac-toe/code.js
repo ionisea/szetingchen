9ebb0ab340076133e1f73e05ba94630bb01c1b22
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-26 15:19:03 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -21,11 +21,17 @@ registerOnclick((x, y) => {
     // this is the lag machine
     let ree = 0;
     let yee = 0;
+    let pee = 0;
+    let lee = 0;
     for (let i = 0; i < 250000; i++) {
       ree = Math.random()
       yee < ree ? yee = ree : 0
     }
-    console.log(yee)
+    for (let i = 0; i < 250000; i++) {
+      pee = Math.random()
+      lee > pee ? lee = pee : 1
+    }
+
   } else {
     y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
     x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
