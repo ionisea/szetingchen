e8cbb07c8d3c8f6f835e67da3a4e9405aee54a54
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-10-28 12:02:18 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -55,6 +55,7 @@ registerOnclick((x, y) => {
     y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
     x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
     if (coordArray[yPos][xPos] == '') {
+      coordArray[yPos][xPos].pop
       coordArray[yPos][xPos].push(player)
       console.log('coords: ', x, y)
       drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3)
