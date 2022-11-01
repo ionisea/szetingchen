fcf3bec579fa78cda080655bea8cf0a1dbf415ca
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-01 14:50:40 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -3,9 +3,9 @@ const player2 = 'O'
 
 drawFilledRect(0, 0, width, height, 'rgba(115,175,255,1)')
 const coordArray = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', ''],
+  [[''], [''], ['']],
+  [[''], [''], ['']],
+  [[''], [''], ['']],
 ]
 const max = Math.max(width, height)
 const min = Math.min(width, height)
@@ -15,27 +15,32 @@ for (let editConst = 1 / 3; editConst < 1; editConst += 1 / 3) {
 }
 
 
+const isGameOver = (array) => {
+  let moves = 0
+  let gameOver = false
+  for (const element of array) {
+    if (element == ! '') {
+      moves += 0
+    } else {
+      moves++
+    }
+  }
+  if (moves = 9) {
+    gameOver = true
+  } else {
+    gameOver = false
+  }
+  if (gameOver = true) {
+    console.log ('ree')
+  }
+}
+
 let xPos;
 let yPos;
 let player = player1;
 registerOnclick((x, y) => {
   if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
-    drawText('🦖 yee', x - height / 3, y + height / 6, 'yellow', height / 2)
-  } else {
-    y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
-    x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
-    if (coordArray[yPos][xPos] == '') {
-      coordArray[yPos][xPos].pop()
-      coordArray[yPos][xPos].push(player);
-      drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3);
-      console.log(coordArray);
-      player == player1 ? player = player2 : player = player1;
-    }
-  }
-});
-
-
-/* //this is the gambling machine
+    /* this is the gambling machine
     let ree = 0;
     let yee = 0;
     let pee = 1;
@@ -50,3 +55,18 @@ registerOnclick((x, y) => {
     }
     console.log(yee/lee)
     */
+    drawText('🦖 yee', x - height / 3, y + height / 6, 'yellow', height / 2)
+  } else {
+    y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
+    x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
+    if (coordArray[yPos][xPos] == '') {
+      coordArray[yPos][xPos].pop()
+      coordArray[yPos][xPos].push(player)
+      console.log('coords: ', x, y)
+      drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3)
+      player == player1 ? player = player2 : player = player1;
+      console.log(coordArray)
+      isGameOver(coordArray);
+    }
+  }
+});
