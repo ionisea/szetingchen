0592435cd2fc39609d26a588da96eebd2172dfeb
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-01 14:59:05 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -44,7 +44,7 @@ registerOnclick((x, y) => {
       drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3);
       console.log(coordArray);
       isGameOver(player);
-      console.log(isGameOver)
+      console.log(isGameOver (player))
       player == player1 ? player = player2 : player = player1;
     }
   }
