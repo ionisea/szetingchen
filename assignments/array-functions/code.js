7a878edb09a3e8d9c4458046f76d5785d4f68c4b
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-02 14:38:27 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -35,4 +35,4 @@ const evens = (array) => array.filter((x) => x % 2 == 0)
 
 const anyOverOneHundred = (array) => array.filter((x) => x > 100).length > 0
 
-const pyramid = (num) => Array((num*(num+1))/2).fill().map
+const pyramid = (num) => Array((num*(num+1))/2).fill().map((_, i) => i)
