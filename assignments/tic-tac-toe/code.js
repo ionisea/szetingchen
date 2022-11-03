88fa1146ac3f21dd1c812955d8379bc58e817d89
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-03 15:26:36 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -3,9 +3,9 @@ const player2 = 'O'
 
 drawFilledRect(0, 0, width, height, 'rgba(115,175,255,1)')
 const coordArray = [
-  [[''], [''], ['']],
-  [[''], [''], ['']],
-  [[''], [''], ['']],
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', ''],
 ]
 const max = Math.max(width, height)
 const min = Math.min(width, height)
@@ -14,21 +14,16 @@ for (let editConst = 1 / 3; editConst < 1; editConst += 1 / 3) {
   drawLine(max / 2 - min / 2, height * editConst, max / 2 + min / 2, height * editConst, 'black', 5)
 }
 
-let winner;
-const isGameOver = (player) => {
-  let winner = player
-  if ((coordArray[0][0] == player && coordArray[1][1] == player && coordArray[2][2] == player) ||
-  (coordArray[2][0] == player && coordArray[1][1] == player && coordArray[0][2] == player) ||
-  (coordArray[0][0] == player && coordArray[0][1] == player && coordArray[0][2] == player) ||
-  (coordArray[1][0] == player && coordArray[1][1] == player && coordArray[1][2] == player) ||
-  (coordArray[2][0] == player && coordArray[2][1] == player && coordArray[2][2] == player) ||
-  (coordArray[0][0] == player && coordArray[1][0] == player && coordArray[2][0] == player) ||
-  (coordArray[0][1] == player && coordArray[1][1] == player && coordArray[2][1] == player) ||
-  (coordArray[0][2] == player && coordArray[1][2] == player && coordArray[2][2] == player)) {
-    return winner
-  } else {
-    return undefined
-  }
+const isGameDraw = (turns) => {
+
+}
+
+const isWinner = () => {
+
+}
+
+const isGameOver = () => {
+  return isWinner || isGameDraw
 }
 
 let xPos;
@@ -40,31 +35,12 @@ registerOnclick((x, y) => {
   } else {
     y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
     x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
-    if (coordArray[yPos][xPos] == '' && winner == undefined) {
-      coordArray[yPos][xPos].pop()
-      coordArray[yPos][xPos].push(player);
+    if (coordArray[yPos][xPos] == '') {
+      coordArray[yPos][xPos] = player
       drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3);
       console.log(coordArray);
-      isGameOver(player);
-      console.log(isGameOver (player))
       player == player1 ? player = player2 : player = player1;
     }
   }
 });
 
-
-/* //this is the gambling machine
-    let ree = 0;
-    let yee = 0;
-    let pee = 1;
-    let lee = 1;
-    for (let i = 0; i < 500000000; i++) {
-      ree = Math.random()
-      yee < ree ? yee = ree : 0
-    }
-    for (let i = 0; i < 500000000; i++) {
-      pee = Math.random()
-      lee > pee &&! pee == 0? lee = pee : 0
-    }
-    console.log(yee/lee)
-    */
