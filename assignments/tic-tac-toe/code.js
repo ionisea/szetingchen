de50020842611d246ef9426161ff691269c8d9d0
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-04 15:02:08 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -27,7 +27,7 @@ const isGameDraw = (array) => {
 }
 
 const isWinner = (array, player) => {
-  for (p = 0; p <= 2; p++) {
+  for (let p = 0; p <= 2; p++) {
     (array[p][0] == player) ? (array[p][1] == player) ? (array[p][2] == player) ? true : false : false : false;
     (array[0][p] == player) ? (array[1][p] == player) ? (array[2][p] == player) ? true : false : false : false;
     (array[0][0] == player) ? (array[1][1] == player) ? (array[2][2] == player) ? true : false : false : false;
