10ea270dd9f34bd6bb571d617e80a9cb45fe9652
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-04 14:57:34 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -14,16 +14,29 @@ for (let editConst = 1 / 3; editConst < 1; editConst += 1 / 3) {
   drawLine(max / 2 - min / 2, height * editConst, max / 2 + min / 2, height * editConst, 'black', 5)
 }
 
-const isGameDraw = (turns) => {
-
+const isGameDraw = (array) => {
+  let count = 0
+  for(let x = 0; x < 3; x++) {
+    for(let y = 0; y < 3; y++) {
+      if (array[x][y] == '') {
+        count++
+      }
+    }
+  }
+  return count > 0
 }
 
-const isWinner = () => {
-
+const isWinner = (array, player) => {
+  for (p = 0; p <= 2; p++) {
+    (array[p][0] == player) ? (array[p][1] == player) ? (array[p][2] == player) ? player : false : false : false;
+    (array[0][p] == player) ? (array[1][p] == player) ? (array[2][p] == player) ? player : false : false : false;
+    (array[0][0] == player) ? (array[1][1] == player) ? (array[2][2] == player) ? player : false : false : false;
+    (array[0][2] == player) ? (array[1][1] == player) ? (array[2][0] == player) ? player : false : false : false;
+  }
 }
 
-const isGameOver = () => {
-  return isWinner || isGameDraw
+const isGameOver = (array, player) => {
+  return isWinner (array, player) || isGameDraw (array)
 }
 
 let xPos;
