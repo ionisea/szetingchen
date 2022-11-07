75559bc3246c02db3bfa713906d9ccf9eefd3b38
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-07 15:01:01 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -13,3 +13,7 @@ const emptyObject = () => {
 const distance = (o1, o2) => {
   return Math.sqrt((o1['y'] - o2['y'])**2 + (o1['x'] - o2['x'])**2)
 }
+
+const midpoint = (o1, o2) => {
+  return {"x" : (o1['x'] + o2['x'])/2, "y" : (o1['y'] + o2['y'])/2}
+}
