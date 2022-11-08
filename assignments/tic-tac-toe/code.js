d976d6daa60dab10cbd3767167a31a9fd8c89ff0
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-08 14:52:14 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -19,7 +19,7 @@ for (let editConst = 1 / 3; editConst < 1; editConst += 1 / 3) {
 
 const winnerLine = (win) => {
   if (win.winType == 'h') {
-    drawLine(leftEnd, )
+    drawLine(leftEnd,)
   }
 
   const isWinner = (player) => {
@@ -38,23 +38,24 @@ const winnerLine = (win) => {
       return { player: player, winType: 'd', winLoca: 1 }
     }
   }
+}
 
-  let xPos;
-  let yPos;
-  let player = player1;
-  let turns = 0
-  registerOnclick((x, y) => {
-    turns++
-    if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
-      drawText('🦖 yee', x - height / 3, y + height / 6, 'yellow', height / 2)
-    } else {
-      y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
-      x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
-      if (coordArray[yPos][xPos] == '' && isWinner(player) == undefined) {
-        coordArray[yPos][xPos] = player
-        drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3);
-        console.log(isWinner(player), turns)
-        player == player1 ? player = player2 : player = player1
-      }
+let xPos;
+let yPos;
+let player = player1;
+let turns = 0
+registerOnclick((x, y) => {
+  turns++
+  if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
+    drawText('🦖 yee', x - height / 3, y + height / 6, 'yellow', height / 2)
+  } else {
+    y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
+    x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
+    if (coordArray[yPos][xPos] == '' && isWinner(player) == undefined) {
+      coordArray[yPos][xPos] = player
+      drawText(player, max / 2 - min / 2 - min * 0.15 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3);
+      console.log(isWinner(player), turns)
+      player == player1 ? player = player2 : player = player1
     }
-  });
+  }
+});
