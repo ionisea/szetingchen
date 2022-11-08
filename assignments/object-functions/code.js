2bc8217a035d7fbf222f079ecf277b24e7bd487f
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-07 16:00:56 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -1,25 +1,8 @@
-const getX = (o) => {
-  return o.x
-}
-
-const point = (n1, n2) => {
-  return { "x": n1, "y": n2 }
-}
-
-const emptyObject = () => {
-  return {}
-}
-
-const distance = (o1, o2) => {
-  return Math.sqrt((o1.y - o2.y) ** 2 + (o1.x - o2.x) ** 2)
-}
-
-const midpoint = (o1, o2) => {
-  return { "x": (o1.x + o2.x) / 2, "y": (o1.y + o2.y) / 2 }
-}
-
+const getX = (o) => o.x
+const point = (n1, n2) => ({ "x": n1, "y": n2 })
+const emptyObject = () => ({})
+const distance = (o1, o2) => Math.sqrt((o1.y - o2.y) ** 2 + (o1.x - o2.x) ** 2)
+const midpoint = (o1, o2) => ({ "x": (o1.x + o2.x) / 2, "y": (o1.y + o2.y) / 2 })
 const sumSalaries = (a) => a.reduce((acc, x) => acc + x.salary, 0)
-
 const newHighScore = (hs, ps) => ps.reduce((acc, p) => Math.max(p.score, acc), hs)
-
 const summarizeBooks = (a) => ({"titles" : a.map(x => x.title), "pages" : a.reduce((acc, x) => acc + x.pages, 0)})
