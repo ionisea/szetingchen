ad2158c4e9d6c6d6642d1a69d5e7df1a09b3e90c
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-09 15:38:46 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -53,17 +53,19 @@ const isWinner = (player) => {
 let xPos;
 let yPos;
 let player = player1;
+let turns = 0
 registerOnclick((x, y) => {
   if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
     drawText('🦖 yee', x - height / 3, y + height / 6, 'yellow', height / 2)
   } else {
     y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
     x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
-    if (coordArray[yPos][xPos] == '' && winner == undefined) {
+    if (coordArray[yPos][xPos] == '' && winner == undefined && turns < 9) {
       coordArray[yPos][xPos] = player
       drawText(player, max / 2 - min / 2 - min / 10 + min / 6 + (min * xPos / 3), min * 0.11 + min / 6 + min * yPos / 3, 'black', min * 0.3);
       winnerLine(isWinner(player))
       player == player1 ? player = player2 : player = player1
+      turns++
     }
   }
 });
