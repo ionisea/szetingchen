5228a963e0d3cfdc4eb4d586523b0cfcc06dc2ad
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-10 15:23:45 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -68,7 +68,7 @@ registerOnclick((x, y) => {
       turns++
       if (winner === undefined && turns === 9) {
         drawFilledRect(0, 0, max, min, 'white')
-        drawText('Draw', 0, 5 * (min / 8), 'black', 3 * (min / 4))
+        drawText('Draw', 0, 5 * (min / 8), 'black', min /2)
       }
     }
   }
