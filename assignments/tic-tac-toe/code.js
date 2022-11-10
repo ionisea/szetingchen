cefab60b5d957d2cfada1ef7df6909aa7575e69a
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-10 07:41:45 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -58,7 +58,7 @@ registerOnclick((x, y) => {
   if (x < max / 2 - min / 2 || x > max / 2 + min / 2) {
     drawText('🦖 yee', x - height / 3, y + height / 6, 'yellow', height / 2)
   } else {
-    y < 1 / 3 * height ? yPos = 0 : y < 2 / 3 * height ? yPos = 1 : yPos = 2;
+    yPos = Math.floor(y / (height / 3))
     x < max / 2 - min / 6 ? xPos = 0 : x < max / 2 + min / 6 ? xPos = 1 : xPos = 2
     if (coordArray[yPos][xPos] == '' && winner == undefined && turns < 9) {
       coordArray[yPos][xPos] = player
