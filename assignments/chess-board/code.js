c884b4d7cc45316b0ae54276e3355e2ea8d523e3
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-11 15:47:58 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -6,10 +6,21 @@ const WHITE_KNIGHT = '♘';
 const WHITE_PAWN   = '♙';
 const BLACK_KING   = '♚';
 const BLACK_QUEEN  = '♛';
-const BLACK_ROOK   = '♜';
+const BLACK_ROOK   = '♛';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
 const BLACK_PAWN   = '♟';
 
 // Example of drawing one of the pieces
 drawText(WHITE_KING, width/2, height/2, 'black', 64);
+
+const chessArray = [
+  ['', '', '', '♛', '♚', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'],
+  ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
+]
