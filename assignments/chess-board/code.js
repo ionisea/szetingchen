0aef3d09594cbfcf6c9158071a7836cf5ee0d085
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-11 16:15:38 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -1,18 +1,15 @@
-const WHITE_KING   = '♔';
-const WHITE_QUEEN  = '♕';
-const WHITE_ROOK   = '♖';
+const WHITE_KING = '♔';
+const WHITE_QUEEN = '♕';
+const WHITE_ROOK = '♖';
 const WHITE_BISHOP = '♗';
 const WHITE_KNIGHT = '♘';
-const WHITE_PAWN   = '♙';
-const BLACK_KING   = '♚';
-const BLACK_QUEEN  = '♛';
-const BLACK_ROOK   = '♜';
+const WHITE_PAWN = '♙';
+const BLACK_KING = '♚';
+const BLACK_QUEEN = '♛';
+const BLACK_ROOK = '♜';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
-const BLACK_PAWN   = '♟';
-
-// Example of drawing one of the pieces
-drawText(WHITE_KING, width/2, height/2, 'black', 64);
+const BLACK_PAWN = '♟';
 
 const chessArray = [
   ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'],
@@ -25,13 +22,27 @@ const chessArray = [
   ['♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'],
 ]
 
-  const bigSquareSide = Math.min(width, height)
-  const squareSide = bigSquareSide / 8
-  const widthExtra = (width - bigSquareSide) / 2
-  const heightExtra = (height - bigSquareSide) / 2
-  drawFilledRect(Math.max(widthExtra, heightExtra), 0, bigSquareSide, bigSquareSide, 'black')
-  for (let i = 0; i < 8; i++) {
-    for (let j = i % 2; j < 8; j += 2) {
-      drawFilledRect(j * squareSide + widthExtra, i * squareSide + heightExtra, squareSide, squareSide, 'white')
-    }
+const boardSide = Math.min(width, height)
+const squareSide = boardSide / 8
+const widthExtra = (width - boardSide) / 2
+const heightExtra = (height - boardSide) / 2
+drawFilledRect(Math.max(widthExtra, heightExtra), 0, boardSide, boardSide, 'black')
+for (let i = 0; i < 8; i++) {
+  for (let j = i % 2; j < 8; j += 2) {
+    drawFilledRect(j * squareSide + widthExtra, i * squareSide + heightExtra, squareSide, squareSide, 'white')
   }
+}
+
+drawText(WHITE_KING, width / 2, height / 2, 'black', 64);
+drawText(WHITE_QUEEN, width / 2, height / 2, 'black', 64);
+drawText(WHITE_ROOK, width / 2, height / 2, 'black', 64);
+drawText(WHITE_KNIGHT, width / 2, height / 2, 'black', 64);
+drawText(WHITE_BISHOP, width / 2, height / 2, 'black', 64);
+drawText(WHITE_PAWN, width / 2, height / 2, 'black', 64)
+
+drawText(BLACK_KING, width / 2, height / 2, 'black', 64);
+drawText(BLACK_QUEEN, width / 2, height / 2, 'black', 64);
+drawText(BLACK_ROOK, width / 2, height / 2, 'black', 64);
+drawText(BLACK_KNIGHT, width / 2, height / 2, 'black', 64);
+drawText(BLACK_BISHOP, width / 2, height / 2, 'black', 64);
+drawText(BLACK_PAWN, width / 2, height / 2, 'black', 64);
