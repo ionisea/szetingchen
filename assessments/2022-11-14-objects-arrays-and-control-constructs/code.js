1ea9292d38e00a46126e04397a01f7f314258326
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-14 15:12:34 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -6,7 +6,11 @@ const area = (rect) => {
 };
 
 const higherPaid = (e1, e2) => {
-  return ({ salary: (Math.max(e1.salary, e2.salary)) })
+  if (e1.salary > e2.salary) {
+    return e1
+  } else {
+    return e2
+  }
 };
 
 const isSamePoint = (p1, p2) => {
