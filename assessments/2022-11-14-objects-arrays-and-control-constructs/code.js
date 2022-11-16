b0397f68a8d20964985322ed05b6f38411a2cdf2
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-16 14:43:47 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -67,6 +67,8 @@ const rule110 = (cells) => {
       array[i] = 0
     } else if (cells[i - 1] === 1 && cells[i] === 1 && cells[i + 1] === 1) {
       array[i] = 0
+    } else {
+      array[i] = 1
     }
   }
   return array
