6f70cf7d93d7d595605796cba83a05e588ec5130
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-11-30 15:06:33 -0800

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -6,23 +6,23 @@ const emit = (a, b, product) => {
 
 // Write your code here ...
 const averageWeight = (totalWeight, numItems) => {
-  return (totalWeight/numItems)
+  return (totalWeight / numItems)
 }
 
 const hypotenuse = (a, b) => {
-  return Math.sqrt(a**2 + b**2)
+  return Math.sqrt(a ** 2 + b ** 2)
 }
 
 const maxRadius = (widthOfArea, heightOfArea) => {
-  return (Math.min (widthOfArea, heightOfArea))/2
+  return (Math.min(widthOfArea, heightOfArea)) / 2
 }
 
 const numCircles = (radius, widthOfArea) => {
-  return Math.floor (widthOfArea/radius)
+  return Math.floor(widthOfArea / radius)
 }
 
 const offset = (widthOfArea, widthOfFigure) => {
-  return (widthOfArea-((Math.floor(widthOfArea/widthOfFigure))*widthOfFigure))/2
+  return (widthOfArea - ((Math.floor(widthOfArea / widthOfFigure)) * widthOfFigure)) / 2
 }
 
 const canSleepIn = (isWeekday, onVacation) => {
@@ -30,7 +30,7 @@ const canSleepIn = (isWeekday, onVacation) => {
 }
 
 const canGoToProm = (isASenior, invitedBySenior, onExclusionList) => {
-  return (isASenior || invitedBySenior) &&! onExclusionList
+  return (isASenior || invitedBySenior) && !onExclusionList
 }
 
 const getsSpeedingTicket = (mph, isCopGrouchy) => {
@@ -50,7 +50,7 @@ const getsSpeedingTicket = (mph, isCopGrouchy) => {
 }
 
 const moreThanTwiceAsLong = (a, b) => {
-  if (a.length > (b.length)*2) {
+  if (a.length > (b.length) * 2) {
     return true
   } else {
     return false
@@ -58,7 +58,7 @@ const moreThanTwiceAsLong = (a, b) => {
 }
 
 const aFartherThanB = (a, b, c) => {
-  if (Math.abs (a-c) > Math.abs (b-c)) {
+  if (Math.abs(a - c) > Math.abs(b - c)) {
     return true
   } else {
     return false
@@ -66,11 +66,11 @@ const aFartherThanB = (a, b, c) => {
 }
 
 const firstHalf = (s) => {
-  return s.substring (0, Math.floor(s.length/2))
+  return s.substring(0, Math.floor(s.length / 2))
 }
 
 const secondHalf = (s) => {
-  return s.substring (Math.floor(s.length/2), s.length)
+  return s.substring(Math.floor(s.length / 2), s.length)
 }
 
 const upDown = (s) => {
@@ -78,15 +78,15 @@ const upDown = (s) => {
 }
 
 const everyOther = (s) => {
-  return s[0]+s[2]+s[4]
+  return s[0] + s[2] + s[4]
 }
 
 const upDownLastCharacter = (s) => {
-  return s[s.length-1].toUpperCase()+s[s.length-1]
+  return s[s.length - 1].toUpperCase() + s[s.length - 1].toLowerCase
 }
 
 const yesIfEven = (num) => {
-  if (num/2 > Math.floor(num/2)) {
+  if (num / 2 > Math.floor(num / 2)) {
     return 'no'
   } else {
     return 'yes'
@@ -106,7 +106,7 @@ const countXs = (s) => {
 const timesTable = (n) => {
   for (let x = 1; x <= n; x++) {
     for (let y = 1; y <= n; y++) {
-      emit(x, y, x*y)
+      emit(x, y, x * y)
     }
   }
 }
@@ -116,5 +116,5 @@ const containsX = (s) => {
 }
 
 const sumSquares = (n) => {
-  return Array(n).fill().map((_, i) => i).reduce((acc, x) => acc + (x**2), 0)
+  return Array(n).fill().map((_, i) => i).reduce((acc, x) => acc + (x ** 2), 0)
 }
