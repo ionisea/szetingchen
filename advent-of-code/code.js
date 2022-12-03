007c81828cd56da8020314c625206a1d69154546
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-02 22:44:45 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -14,11 +14,34 @@
 // return value in the REPL.
 
 const extract = (s) => s.split('\n')
-
 const day01Part1 = (s) => {
   let currentSum = 0
   let max = 0
-
 }
 
 console.log(run ('day01.sample', extract))
+
+/* (done in jsfiddle)
+let max1 = 0
+let max2 = 0
+let max3 = 0
+for (let i in input) {
+	let sum = 0
+	input[i] = input[i].split('\n').map(Number)
+  for (let j = 0; j < input[i].length; j++) {
+  sum += input[i][j]
+	}
+  if (sum > max1) {
+  max3 = max2
+  max2 = max1
+  max1 = sum
+  } else if (sum > max2) {
+  max2 = max3
+  max2 = sum
+  } else if (sum > max3) {
+  max3 = sum
+  }
+}
+
+console.log(max1 + max2 + max3)
+*/
