0bab1dde90d2a23eeab8358c45f42a1d80a99b1c
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-03 13:33:54 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -1,18 +1,3 @@
-// Write your code here. To run a function on a given test file you need to
-// create a file in your github repo in the advent-of-code branch and in the
-// advent-of-code directory. Then you can use a function `run` to run a
-// particular function on the contents of the file, which will be passed to your
-// function as a string.
-//
-// For example, if I've created a file 'day_01.test' to contain the test data
-// from day 1, and a function, day01Part1, I can run the function with this
-// call:
-//
-//   run('day_01.test', day01Part1)
-//
-// Which will load the file and pass them to your function and then print the
-// return value in the REPL.
-
 const day01Extract = (s) => s.split('\n\n')
 const day01Part1 = (s) => {
   let input = day01Extract(s)
@@ -119,67 +104,24 @@ const day03Part1 = (s) => {
   let input = day03Extract(s)
   const compartment1 = []
   const compartment2 = []
+  const letters = []
   for (let i in input) {
     compartment1.push(input[i].substring(0, input[i].length / 2))
     compartment2.push(input[i].length / 2)
   }
   for (let x = 0; x < compartment1.length; x++) {
-    //find if any letter matches (case sensitive)
+    for (let y = 0; y < compartment1[x].length; y++) {
+      for (let z = 0; z < compartment2[x].length; z++) {
+        if (compartment1[x][y] === compartment2[x][z]) {
+          letters.push(compartment2[x][z])
+        }
+      }
+    }
   }
+  
 }
 
 run('day01.input', day01Part1, 69177)
 run('day01.input', day01Part2, 207456)
 run('day02.input', day02Part1, 8890)
 run('day02.input', day02Part2, 10238)
-
-/* Day 2 Part 1: (Done in jsFiddle)
-let points = 0
-for (let i in input) {
-  if (input[i][0] === "A" && input[i][1] === "X") {
-    points += (1 + 3)
-  } else if (input[i][0] === "A" && input[i][1] === "Y") {
-    points += (2 + 6)
-  } else if (input[i][0] === "A" && input[i][1] === "Z") {
-    points += (3 + 0)
-  } else if (input[i][0] === "B" && input[i][1] === "X") {
-    points += (1 + 0)
-  } else if (input[i][0] === "B" && input[i][1] === "Y") {
-    points += (2 + 3)
-  } else if (input[i][0] === "B" && input[i][1] === "Z") {
-    points += (3 + 6)
-  } else if (input[i][0] === "C" && input[i][1] === "X") {
-    points += (1 + 6)
-  } else if (input[i][0] === "C" && input[i][1] === "Y") {
-    points += (2 + 0)
-  } else if (input[i][0] === "C" && input[i][1] === "Z") {
-    points += (3 + 3)
-  }
-}
-console.log(points)
-
-Day 2 Part 2: (done in jsFiddle)
-let points = 0
-for (let i in input) {
-  if (input[i][0] === "A" && input[i][1] === "X") {
-    points += (3 + 0)
-  } else if (input[i][0] === "A" && input[i][1] === "Y") {
-    points += (1 + 3)
-  } else if (input[i][0] === "A" && input[i][1] === "Z") {
-    points += (2 + 6)
-  } else if (input[i][0] === "B" && input[i][1] === "X") {
-    points += (1 + 0)
-  } else if (input[i][0] === "B" && input[i][1] === "Y") {
-    points += (2 + 3)
-  } else if (input[i][0] === "B" && input[i][1] === "Z") {
-    points += (3 + 6)
-  } else if (input[i][0] === "C" && input[i][1] === "X") {
-    points += (2 + 0)
-  } else if (input[i][0] === "C" && input[i][1] === "Y") {
-    points += (3 + 3)
-  } else if (input[i][0] === "C" && input[i][1] === "Z") {
-    points += (1 + 6)
-  }
-}
-console.log(points)
-*/
