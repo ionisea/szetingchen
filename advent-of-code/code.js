6fde8ec2b6b3f225e620c6ddb7abcd9fce4e60aa
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-03 11:33:28 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -86,7 +86,7 @@ const day02Part1 = (s) => {
 
 run('day01.input', day01Part1, 69177)
 run('day01.input', day01Part2, 207456)
-run('day02.input', day02Part1)
+run('day02.input', day02Part1, 8890)
 
 /* Day 2 Part 1: (Done in jsFiddle)
 let points = 0
