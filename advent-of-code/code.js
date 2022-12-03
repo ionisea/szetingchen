e5e2107efa3ebf087446ee016a19b8e158a49429
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-03 11:29:56 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -28,7 +28,7 @@ const day01Part1 = (s) => {
     }
   }
   return max
-} 
+}
 
 const day01Part2 = (s) => {
   let input = day01Extract(s)
@@ -55,34 +55,38 @@ const day01Part2 = (s) => {
   return max1 + max2 + max3
 }
 
-run('day01.input', day01Part1, 69177)
-run('day01.input', day01Part2, 207456)
 
-/* Day 1: (done in jsfiddle)
-let max1 = 0
-let max2 = 0
-let max3 = 0
-for (let i in input) {
-  let sum = 0
-  input[i] = input[i].split('\n').map(Number)
-  for (let j = 0; j < input[i].length; j++) {
-  sum += input[i][j]
+const day02Extract = (s) => s.split('\n').map(i => i.split(''))
+const day02Part1 = (s) => {
+  let input = day02Extract(s)
+  let points = 0
+  for (let i in input) {
+    if (input[i][0] === "A" && input[i][1] === "X") {
+      points += (1 + 3)
+    } else if (input[i][0] === "A" && input[i][1] === "Y") {
+      points += (2 + 6)
+    } else if (input[i][0] === "A" && input[i][1] === "Z") {
+      points += (3 + 0)
+    } else if (input[i][0] === "B" && input[i][1] === "X") {
+      points += (1 + 0)
+    } else if (input[i][0] === "B" && input[i][1] === "Y") {
+      points += (2 + 3)
+    } else if (input[i][0] === "B" && input[i][1] === "Z") {
+      points += (3 + 6)
+    } else if (input[i][0] === "C" && input[i][1] === "X") {
+      points += (1 + 6)
+    } else if (input[i][0] === "C" && input[i][1] === "Y") {
+      points += (2 + 0)
+    } else if (input[i][0] === "C" && input[i][1] === "Z") {
+      points += (3 + 3)
+    }
   }
-  if (sum > max1) {
-  max3 = max2
-  max2 = max1
-  max1 = sum
-  } else if (sum > max2) {
-  max2 = max3
-  max2 = sum
-  } else if (sum > max3) {
-  max3 = sum
-  }
+  return points
 }
 
-Part 1: console.log(max1)
-Part 2: console.log(max1 + max2 + max3)
-*/
+run('day01.input', day01Part1, 69177)
+run('day01.input', day01Part2, 207456)
+run('day02.input', day02Part1)
 
 /* Day 2 Part 1: (Done in jsFiddle)
 let points = 0
