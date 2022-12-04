7ff6b4f499798f553efe537cfbc1dc647f75bc6b
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-03 19:27:55 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -110,15 +110,18 @@ const day03Part1 = (s) => {
     compartment2.push(input[i].length / 2)
   }
   for (let x = 0; x < compartment1.length; x++) {
+    let toPush = []
     for (let y = 0; y < compartment1[x].length; y++) {
       for (let z = 0; z < compartment2[x].length; z++) {
         if (compartment1[x][y] === compartment2[x][z]) {
-          letters.push(compartment2[x][z])
+          toPush.push(compartment2[x][z])
         }
       }
     }
+    
+    letters.push(toPush)
+    toPush = Array(0)
   }
-  
 }
 
 run('day01.input', day01Part1, 69177)
