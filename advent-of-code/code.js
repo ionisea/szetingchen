0f48da43360a3cd7f38fc8310da1c061139fe583
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-05 15:14:07 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -121,7 +121,7 @@ const day03Part1 = (s) => {
       items[i] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(items[i]) + 27
     }
   }
-  return items[3]
+  return items[4]
 }
 
 run('day01.input', day01Part1, 69177)
