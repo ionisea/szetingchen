6514baf2b67ecc2359565353a4a38fb428569e90
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-05 20:43:12 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -111,6 +111,7 @@ const commonItem = (a) => {
   for (let i = 1; i < a.length - 1; a++) {
     a[i] = [...a[i]].filter(x => x.includes(a[i - 1]))
   }
+  console.log(a[0])
   return a[0]
 }
 const day03Part1 = (s) => {
