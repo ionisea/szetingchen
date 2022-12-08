06502c2e0d68995ca19cd07be73aff87e616f6be
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-08 15:25:58 -0800

    Updating advent-of-code/code.js

--- advent-of-code/code.js
+++ advent-of-code/code.js
@@ -172,5 +172,5 @@ const add2Vectors = (a) => {
 }
 
 const addNumVectors = (a) => {
-  return a.reduce((acc, x) => acc = add2Vectors([acc, x]), vector(0, 0))
+  return a.reduce((acc, x) => add2Vectors([acc, x]), vector(0, 0))
 }
