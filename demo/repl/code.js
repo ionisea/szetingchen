25381c203e896d94824512294de507eaac9f00ff
szetingchen <111320652+szetingchen@users.noreply.github.com>
2022-12-12 15:24:40 -0800

    Updating demo/repl/code.js

--- demo/repl/code.js
+++ demo/repl/code.js
@@ -1,3 +1,4 @@
+/*
 // Warning! Do not call this function with numbers much bigger than 40 unless
 // you want to kill this tab.
 const fib = (n) => (n < 2 ? n : fib(n - 2) + fib(n - 1));
@@ -16,3 +17,56 @@ const fib2 = (n) => {
 const MAX_FIB_N = 1476;
 
 const MAX_FIB = fib2(MAX_FIB_N);
+
+*/
+
+
+const vector = (angle, magnitude) => {
+  return ({ angle: angle * Math.PI / 180, magnitude })
+}
+
+const add2Vectors = (a) => {
+  const x1 = Math.cos(a[0].angle) * a[0].magnitude
+  const x2 = Math.cos(a[1].angle) * a[1].magnitude
+  const y1 = Math.sin(a[0].angle) * a[0].magnitude
+  const y2 = Math.sin(a[1].angle) * a[1].magnitude
+  const angle = Math.atan2(y1 + y2, x1 + x2)
+  const mag = Math.sqrt((x1 + x2) ** 2 + (y1 + y2) ** 2)
+  return ({ angle, magnitude: mag })
+}
+
+const vectorMultiply = (o, n) => {
+  if (n >= 0) {
+    return ({ angle: o.angle, magnitude: o.magnitude * n })
+  } else {
+    return ({ angle: (o.angle + Math.PI) % (2 * Math.PI), magnitude: o.magnitude * -n })
+  }
+}
+
+const addNumVectors = (a, mode) => {
+  if (mode === 'degrees') {
+    const r = a.reduce((acc, x) => add2Vectors([acc, x]), vector(0, 0))
+    r.angle = r.angle * 180 / Math.PI
+    return r
+  } else {
+    return a.reduce((acc, x) => add2Vectors([acc, x]), vector(0, 0))
+  }
+}
+
+const EARTH_GRAVITY = 9.8
+const G = 6.6743e-11
+
+const gravAttraction = (o1, o2) => {
+  const distance = Math.hypot(Math.abs(o1.position.x - o2.position.x), Math.abs(o1.position.y - o2.position.y))
+  return (o1.mass * o2.mass * G) / distance ** 2
+}
+
+const sampleObject = { mass: 10, x: 100, y: 200}
+
+//Vector is force
+
+const objectNextPos = (object, forceVector) => {
+  const acceleration = forceVector.magnitude/object.mass
+  const velocity = currentVelo + acceleration
+}
+
