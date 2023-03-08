7e455ced3f1bcc5ddcab1b795896b52ce11affe3
szetingchen <111320652+szetingchen@users.noreply.github.com>
2023-03-08 22:28:42 +0000

    start

---
+++ projects/genetic-algorithm/public/scripts/genetics.js
@@ -0,0 +1,38 @@
+const targetString = "To be or not to be, that is the question."
+
+const fitness = (string) => {
+    const fitness = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === targetString[i]) {
+            fitness++
+        };
+    };
+    return { string, fitness }
+};
+
+const randomCharacterGenerator = () => {
+    const randomNumber = Math.random();
+    const characters = "abcdefghijlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;-=' ";
+    const interval = 1 / characters.length;
+    for (let i = 1; i <= characters.length; i++) {
+        if (randomNumber < i * interval) {
+            return characters[i];
+        };
+    };
+};
+
+const firstGeneration = [];
+const firstGenerationGeneration = (string, numOfCases) => {
+    const cases = [];
+    for (let i = 0; i < numOfCases; i++) {
+        const letters = [];
+        for (let j = 0; j < string.length; j++) {
+            letters.push(randomCharacterGenerator);
+        };
+        cases.push(letters.reduce((str, s) => str.concat(s), ''));
+    };
+    return cases;
+};
+
+//previousGen is an array
+const nextGenerationGeneration = (previousGen)
