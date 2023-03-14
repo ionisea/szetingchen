76ed77792dc4bcbfdd7f2c0df8c67a056d633757
szetingchen <111320652+szetingchen@users.noreply.github.com>
2023-03-14 21:46:04 +0000

    a few thingd

--- projects/genetic-algorithm/public/scripts/genetics.js
+++ projects/genetic-algorithm/public/scripts/genetics.js
@@ -1,41 +1,59 @@
 const targetString = "To be or not to be, that is the question."
+const characters = "abcdefghijlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;-=' ";
 
 const fitness = (string) => {
     const fitness = 0;
     for (let i = 0; i < string.length; i++) {
         if (string[i] === targetString[i]) {
-            fitness++
+            fitness++;
         };
     };
     return { string, fitness }
 };
 
-const randomCharacterGenerator = () => {
-    const randomNumber = Math.random();
-    const characters = "abcdefghijlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,./;-=' ";
-    const interval = 1 / characters.length;
-    for (let i = 1; i <= characters.length; i++) {
-        if (randomNumber < i * interval) {
-            return characters[i];
+//cases being an array of strings with the string and fitness
+/*
+const quickSort = (cases) => {
+    if (cases.length <= 1) {
+        return cases;
+    };
+    const pivot = cases[Math.floor(Math.random() * cases.length)];
+    const left = [];
+    const right = [];
+    for (let i = 0; i < cases.length; i++) {
+        if (cases[i] > pivot) {
+            left.push(cases[i]);
+        } else if (cases[i] < pivot) {
+            right.push(cases[i]);
         };
     };
+    return quickSort(left).concat([pivot], quickSort(right));
+};
+*/
+
+
+const randomCharacterGenerator = (string) => {
+    const index = Math.floor(Math.random() * string.length);
+    return string[index];
 };
 
-const firstGenerationGeneration = (string, numOfCases) => {
-    const firstGeneration = [];
+const zeroGeneration = (string, numOfCases) => {
+    const zeroGen = [];
     const cases = [];
     for (let i = 0; i < numOfCases; i++) {
         const letters = [];
         for (let j = 0; j < string.length; j++) {
-            letters.push(randomCharacterGenerator);
+            letters.push(randomCharacterGenerator(characters));
         };
-        cases.push(letters.reduce((str, s) => str.concat(s), ''));
+        cases.push(letters.join(''));
     };
     for (i = 0; i < cases.length; i++) {
-        firstGeneration.push(fitness(cases[i]));
+        zeroGen.push(fitness(cases[i]));
     };
-    return firstGeneration;
+    return zeroGen;
 };
 
 //previousGen is an array
-const nextGenerationGeneration = (previousGen)
+const nextGeneration = (previousGen) => {
+
+}
